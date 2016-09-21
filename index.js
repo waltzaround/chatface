6456a83d930f4a2205f99d1a8432f36ed3f5a65d
Walter Lim <vxs4050@autuni.ac.nz>
2016-09-21 15:43:41 +1200

    updated bot to talk crap

--- index.js
+++ index.js
@@ -7,27 +7,65 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-//Process application/x-www-form-urlencoded
+// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
-//Process application/json
+// Process application/json
 app.use(bodyParser.json())
 
-//index route
+// Index route
 app.get('/', function (req, res) {
-	res.send('Hello world, I am a chat bot.')
+    res.send('Hello world, I am a chat bot')
 })
 
-//for Facebook verification
+// for Facebook verification
 app.get('/webhook/', function (req, res) {
-	if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send('Error, wrong token.')
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
 })
 
-//Spin up the server
+// Spin up the server
 app.listen(app.get('port'), function() {
-	console.log('running on port', app.get('port'))
+    console.log('running on port', app.get('port'))
 })
+
+//Begin an API endpoint. Yay!
+app.post('/webhook/', function (req, res) {
+	let messaging_events = req.body.entry[0].messaging_events
+	for(let i=0; i < messaging_events.length; i++) {
+		let event = req.body.entry[0].messaging[i]
+		let sender = event.sender.id
+		if (event.message && event.message.text) {
+			sendTextMessage(sender, "Text received, echo: " + text.substring (0,200))
+		}
+	}
+	res.sendStatus(200)
+})
+
+const token = "EAAEOy9TA4voBAOL6o8C0hb283iBZB6xvZCdOR0US4NP87JPGdE0dzZCuoB2ZAZB3WHqW771jHnFtWHaOjmyuOVTaVPu1XgbR1JdBG4uN8JAokM633NSgu68lZBXMu8hAYYhbKFRIQwgbNVtmdMkRVvqKZC4VgS3CeaAKSZBlPp8AgAZDZD"
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	request ({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+
+
+
 
